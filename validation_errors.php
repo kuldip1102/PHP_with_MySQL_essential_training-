ad7d43eab83130afe5bd1daae80ce5bf51beb7d5
koolchantu <kuldip.suthar1102@gmail.com>
2017-08-19 20:15:41 +0530

    validaiton_error.php : validation error cut the function of and put the validation function and include the file into the validation_error.php

--- validation_errors.php
+++ validation_errors.php
@@ -5,81 +5,21 @@
 </head>
 <body>
 	<?php 
+		require_once('validation_function.php');
+		$errors = array();
 
-	$errors = array();
+		$username = trim("kuldip");
 
-	// * Presence
-		$value = trim("");
-		if (!isset($value) || $value === "") {
-			$errors['value'] = "value can't be blank.";
+		if (!has_presence($username)) {
+			$errors['value'] = "Usename can't be blank";
 		}
 
-	// * String length 
-		$value = "asedfs";
-		$min = 3;
-		$max = 6;
-		if (strlen($value) < $min) {
-			echo "Validation min.";
-		}
-		if (strlen($value) >  $max ) {
-			echo "Validation max.";
-		}
-
-	// * type
-		$value = '1';
-		if (!is_string($value)) {
-				echo "validation is failed is not string";
-			}	
-
-	// * inclusion in a set
-		$find = 10;
-		$set = array(10,20,30,40,50);
-		if (!in_array($find, $set)) {
-			echo $find." is not in array.";
-		}
-
-	// * uniqueness
-	
-	// * format
- 		// Use regex on a string.
- 		// pregmatch($regex,$subject)
-/*	if (preg_match("/php/","php is fun.")) {
-		echo "A match is found.";
-	}else{
-		echo "A match is not found.";
-	}
-*/
-	// print_r($errors);
-	?>
-
-	<?php
-/*		if (!empty($errors)) {
-			// redirect_to("basic.php");
-			include('form.php');
-		}else{
-			redirect_to('success.php');
-		}
-*/
-
-	function form_error($errors = array()){
-		$output = "";
-		if (!empty($errors)) {
-			$output .= "<div class=\"error\">";
-			$output .= "Please fix the following errors:";
-			$output .= "<ul>";
-			foreach ($errors as $key => $error) {
-				$output .= "<li>{$error}</li>";
-			}
-			$output .="</ul>";
-			$output .="<div>";
-		}
-		return $output;
-	}
-		
 	?>
 
 	<?php 
+		
 		echo form_error($errors);
+	
 	?>
 </body>
 </html>
