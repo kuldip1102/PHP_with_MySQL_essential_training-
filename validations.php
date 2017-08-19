e2d56a27c7f0cdb742fbc044e37df814ae7208cf
koolchantu <kuldip.suthar1102@gmail.com>
2017-08-19 18:07:14 +0530

    validation.php : just introduction of the basic validations with if else statements.

---
+++ validations.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Validations</title>
+</head>
+<body>
+	<?php 
+
+	// * Presence
+		$value = "d";
+		if (!isset($value) || empty($value)) {
+			echo "validation failed.<br>";
+		}
+
+	// * String length 
+		$value = "asedfs";
+		$min = 3;
+		$max = 6;
+		if (strlen($value) < $min) {
+			echo "Validation min.";
+		}
+		if (strlen($value) >  $max ) {
+			echo "Validation max.";
+		}
+
+	// * type
+		$value = '1';
+		if (!is_string($value)) {
+				echo "validation is failed is not string";
+			}	
+
+	// * inclusion in a set
+		$find = 10;
+		$set = array(10,20,30,40,50);
+		if (!in_array($find, $set)) {
+			echo $find." is not in array.";
+		}
+
+	// * uniqueness
+	
+	// * format
+ 		// Use regex on a string.
+ 		// pregmatch($regex,$subject)
+	if (preg_match("/php/","php is fun.")) {
+		echo "A match is found.";
+	}else{
+		echo "A match is not found.";
+	}
+
+
+	?>
+</body>
+</html>
