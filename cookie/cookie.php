a721ae9aa3b41898adf0486977f82c483c6c02c3
koolchantu <kuldip.suthar1102@gmail.com>
2017-08-20 10:05:21 +0530

    cookie.php : unset the cookie are two ways of the given in the file

--- cookie/cookie.php
+++ cookie/cookie.php
@@ -4,6 +4,13 @@
 		$value = 45;
 		$expire = time()+(60*60);			
 		// setcookie($name,$value, $expire);
+		
+
+		// setcookie($name,null, $expire);
+		// setcookie($name,null);
+		setcookie($name,null, time() - 3600);
+
+
 
 	?>
 	
@@ -16,8 +23,8 @@
 	
 	<pre>
 		<?php
-		// print_r($_COOKIE);
-		 $_COOKIE["kuldip"];
+		$cookie  = isset($_COOKIE[$name]) ? $_COOKIE[$name] : "";
+		echo $cookie;
 		?>
 	</pre>
 
